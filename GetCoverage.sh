a488841a6bc406bf1e7b94c400ec6dd57619ad36
Steve Smith <steve@kentsmiths.com>
2019-08-16 13:28:36 -0400

    Changing quotes in command line

--- GetCoverage.sh
+++ GetCoverage.sh
@@ -1,1 +1,1 @@
-reportgenerator "-reports:.\TestResults\coverage.integration.cobertura.xml;.\TestResults\coverage.unit.cobertura.xml" "-targetdir:CodeCoverage -reportTypes:HtmlInline_AzurePipelines;Cobertura -assemblyfilters:'-xunit*'"
+reportgenerator "-reports:.\TestResults\coverage.integration.cobertura.xml;.\TestResults\coverage.unit.cobertura.xml" "-targetdir:CodeCoverage" "-reportTypes:HtmlInline_AzurePipelines;Cobertura" "-assemblyfilters:'-xunit*'"
