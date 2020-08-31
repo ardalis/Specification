e86209ead407507f114a76aa207bc8a88f86eb58
Fati Iseni <fati.iseni@pozitrongroup.com>
2020-08-31 22:55:08 +0200

    Added customs exceptions for duplicate use of Take and Skip.

---
+++ ArdalisSpecification/src/Ardalis.Specification/Exceptions/DuplicateTakeException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ardalis.Specification.Exceptions
+{
+    public class DuplicateTakeException : Exception
+    {
+        private const string message = "Duplicate use of Take(). Ensure you don't use both Paginate() and Take() in the same specification!";
+
+        public DuplicateTakeException()
+            : base(message)
+        {
+        }
+
+        public DuplicateTakeException(Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
