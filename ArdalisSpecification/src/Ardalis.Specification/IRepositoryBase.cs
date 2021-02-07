c20905b3336a07f3c5e0cc3ecf40b5983006472f
Conner Orth <10193903+ConnerOrth@users.noreply.github.com>
2021-02-07 12:12:08 +0100

    Started #33.
    Added xml comments for IRepositoryBase.

--- ArdalisSpecification/src/Ardalis.Specification/IRepositoryBase.cs
+++ ArdalisSpecification/src/Ardalis.Specification/IRepositoryBase.cs
@@ -3,23 +3,129 @@ using System.Threading.Tasks;
 
 namespace Ardalis.Specification
 {
+    /// <summary>
+    /// <para>
+    /// A <see cref="IRepositoryBase{T}" /> can be used to query and save instances of <typeparamref name="T" />.
+    /// An <see cref="ISpecification{T}"/> (or derived) is used to encapsulate the LINQ queries against the database.
+    /// </para>
+    /// </summary>
+    /// <typeparam name="T"> The type of entity being operated on by this repository.</typeparam>
     public interface IRepositoryBase<T> where T : class
     {
+        /// <summary>
+        /// Adds an entity in the database.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains the <typeparamref name="T" />.
+        /// </returns>
         Task<T> AddAsync(T entity);
+        /// <summary>
+        /// Updates an entity in the database
+        /// </summary>
+        /// <param name="entity">The entity to update.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         Task UpdateAsync(T entity);
+        /// <summary>
+        /// Removes an entity in the database
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         Task DeleteAsync(T entity);
+        /// <summary>
+        /// Removes the given entities in the database
+        /// </summary>
+        /// <param name="entities">The entities to remove.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         Task DeleteRangeAsync(IEnumerable<T> entities);
+        /// <summary>
+        /// Persists changes to the database.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         Task SaveChangesAsync();
 
+        /// <summary>
+        /// Finds an entity with the given primary key value.
+        /// </summary>
+        /// <param name="id">The value of the primary key for the entity to be found.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains the <typeparamref name="T" />, or <see langword="null"/>.
+        /// </returns>
         Task<T?> GetByIdAsync(int id);
+        /// <summary>
+        /// Finds an entity with the given primary key value.
+        /// </summary>
+        /// <typeparam name="TId">The type of primary key.</typeparam>
+        /// <param name="id">The value of the primary key for the entity to be found.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains the <typeparamref name="T" />, or <see langword="null"/>.
+        /// </returns>
         Task<T?> GetByIdAsync<TId>(TId id);
+        /// <summary>
+        /// Finds an entity that matches the encapsulated query logic of the <paramref name="specification"/>.
+        /// </summary>
+        /// <param name="specification">The encapsulated query logic.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains the <typeparamref name="T" />, or <see langword="null"/>.
+        /// </returns>
         Task<T?> GetBySpecAsync(ISpecification<T> specification);
+        /// <summary>
+        /// Finds an entity that matches the encapsulated query logic of the <paramref name="specification"/>.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="specification">The encapsulated query logic.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains the <typeparamref name="TResult" />.
+        /// </returns>
         Task<TResult> GetBySpecAsync<TResult>(ISpecification<T, TResult> specification);
 
+        /// <summary>
+        /// Finds all entities of <typeparamref name="T" /> from the database.
+        /// </summary>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains a <see cref="List{T}" /> that contains elements from the input sequence.
+        /// </returns>
         Task<List<T>> ListAsync();
+        /// <summary>
+        /// Finds all entities of <typeparamref name="T" />, that matches the encapsulated query logic of the
+        /// <paramref name="specification"/>, from the database.
+        /// </summary>
+        /// <param name="specification">The encapsulated query logic.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains a <see cref="List{T}" /> that contains elements from the input sequence.
+        /// </returns>
         Task<List<T>> ListAsync(ISpecification<T> specification);
+        /// <summary>
+        /// Finds all entities of <typeparamref name="T" />, that matches the encapsulated query logic of the
+        /// <paramref name="specification"/>, from the database.
+        /// <para>
+        /// Projects each entity into a new form, being <typeparamref name="TResult" />.
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TResult">The type of the value returned by the projection.</typeparam>
+        /// <param name="specification">The encapsulated query logic.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains a <see cref="List{TResult}" /> that contains elements from the input sequence.
+        /// </returns>
         Task<List<TResult>> ListAsync<TResult>(ISpecification<T, TResult> specification);
 
+        /// <summary>
+        /// Returns a number that represents how many entities satisfy the encapsulated query logic
+        /// of the <paramref name="specification"/>.
+        /// </summary>
+        /// <param name="specification">The encapsulated query logic.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation. The task result contains the
+        /// number of elements in the input sequence.
+        /// </returns>
         Task<int> CountAsync(ISpecification<T> specification);
     }
 }
