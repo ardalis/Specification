8b84f6f988de70e430064240fb3ad9c5599a33f4
Fati Iseni <fati.iseni@pozitrongroup.com>
2021-02-26 22:09:04 +0100

    Fix the namespace for DbSetExtensions.

--- ArdalisSpecificationEF/src/Ardalis.Specification.EF/Extensions/DbSetExtensions.cs
+++ ArdalisSpecificationEF/src/Ardalis.Specification.EF/Extensions/DbSetExtensions.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Ardalis.Specification.EntityFrameworkCore.Extensions
+namespace Ardalis.Specification.EntityFrameworkCore
 {
     public static class DbSetExtensions
     {
