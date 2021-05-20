f47385ca3148b3e3f253e4d4fe54d4481ffef811
Frank Wagner <f.wagner@wagnersolutions.de>
2021-05-20 19:55:31 +0200

    added interface ISingleResultSpecification<T> (#127)

---
+++ ArdalisSpecification/src/Ardalis.Specification/ISingleResultSpecificationOfT.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ardalis.Specification
+{
+    public interface ISingleResultSpecification<T> : ISpecification<T>, ISingleResultSpecification
+    {
+
+    }
+}
