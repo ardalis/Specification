ad59c4a03425fc5f63094af922028cb2975c5b4a
Fati Iseni <fati.iseni@pozitrongroup.com>
2021-09-08 00:49:16 +0200

    Added tests for in-memory search.

---
+++ Specification/tests/Ardalis.Specification.UnitTests/EvaluatorTests/SearchEvaluator_Evaluate.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ardalis.Specification.UnitTests
+{
+    public class SearchEvaluator_Evaluate
+    {
+        public static List<Person> Data = new List<Person>
+        {
+            new Person("James"),
+            new Person("Robert"),
+            new Person("Mary"),
+            new Person("Linda"),
+            new Person("Michael"),
+            new Person("David"),
+        };
+
+        [Theory]
+        [InlineData("%mes", 1)]
+        [InlineData("%r%", 2)]
+        [InlineData("_inda", 1)]
+        [InlineData("M%", 2)]
+        [InlineData("[RM]%", 3)]
+        [InlineData("_[IA]%", 5)]
+        public void ReturnsFilteredList_GivenSearchExpression(string searchTerm, int expectedCount)
+        {
+            var result = SearchEvaluator.Instance.Evaluate(Data, new PersonSpecification(searchTerm));
+
+            result.Should().HaveCount(expectedCount);
+        }
+    }
+
+    public class PersonSpecification : Specification<Person>
+    {
+        public PersonSpecification(string searchTerm)
+        {
+            Query.Search(x => x.Name, searchTerm);
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Specification/tests/Ardalis.Specification.UnitTests/ExceptionTests/InvalidSearchPatternExceptionTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Ardalis.Specification.UnitTests
+{
+    public class InvalidSearchPatternExceptionTests
+    {
+        private const string defaultMessage = "Invalid search pattern: " + pattern;
+        private const string pattern = "x";
+
+        [Fact]
+        public void ThrowWithDefaultConstructor()
+        {
+            Action action = () => throw new InvalidSearchPatternException(pattern);
+
+            action.Should().Throw<InvalidSearchPatternException>(pattern).WithMessage(defaultMessage);
+        }
+
+        [Fact]
+        public void ThrowWithInnerException()
+        {
+            Exception inner = new Exception("test");
+            Action action = () => throw new InvalidSearchPatternException(pattern, inner);
+
+            action.Should().Throw<InvalidSearchPatternException>(pattern).WithMessage(defaultMessage).WithInnerException<Exception>().WithMessage("test");
+        }
+    }
+}
