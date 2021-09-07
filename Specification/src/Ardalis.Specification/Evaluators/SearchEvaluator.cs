53ed8bb03f2bbba80d6ebc1a2dd1ff35f64b731c
Fati Iseni <fati.iseni@pozitrongroup.com>
2021-09-08 00:49:00 +0200

    Added in-memory search feature.

---
+++ Specification/src/Ardalis.Specification/Evaluators/SearchEvaluator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ardalis.Specification
+{
+    public class SearchEvaluator : IInMemoryEvaluator
+    {
+        private SearchEvaluator() { }
+        public static SearchEvaluator Instance { get; } = new SearchEvaluator();
+
+        public IEnumerable<T> Evaluate<T>(IEnumerable<T> query, ISpecification<T> specification)
+        {
+            foreach (var searchGroup in specification.SearchCriterias.GroupBy(x => x.SearchGroup))
+            {
+                var criterias = searchGroup.Select(x => (x.Selector, x.SearchTerm));
+
+                query = query.Where(x => criterias.Any(c => c.Selector.Compile()(x).Like(c.SearchTerm)));
+            }
+
+            return query;
+        }
+    }
+}
---
+++ Specification/src/Ardalis.Specification/Exceptions/InvalidSearchPatternException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ardalis.Specification
+{
+    public class InvalidSearchPatternException : Exception
+    {
+        private const string message = "Invalid search pattern: ";
+
+        public InvalidSearchPatternException(string searchPattern)
+            : base($"{message}{searchPattern}")
+        {
+        }
+
+        public InvalidSearchPatternException(string searchPattern, Exception innerException)
+            : base($"{message}{searchPattern}", innerException)
+        {
+        }
+    }
+}
