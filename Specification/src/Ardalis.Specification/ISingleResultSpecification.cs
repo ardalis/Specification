bda0a959408530f048147e849d538dbf4daf392b
Mustafa.Elshobaky <mustafa.elshobaky@integrant.com>
2021-10-30 20:47:34 +0200

    Add xml comments for Specification classes

--- Specification/src/Ardalis.Specification/ISingleResultSpecification.cs
+++ Specification/src/Ardalis.Specification/ISingleResultSpecification.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Ardalis.Specification
+﻿namespace Ardalis.Specification
 {
+    /// <summary>
+    /// A marker interface for specifications that are meant to return a single entity
+    /// </summary>
     public interface ISingleResultSpecification
     {
     }
