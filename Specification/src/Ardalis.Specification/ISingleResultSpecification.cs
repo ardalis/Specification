39fe243fe1db65253adfaabafa705c1903c4c4b1
mustafaelshobaky <dr.mustafa.elshobaky@gmail.com>
2021-10-30 21:47:38 +0200

    Update Specification/src/Ardalis.Specification/ISingleResultSpecification.cs
    Co-authored-by: Steve Smith <steve@kentsmiths.com>

--- Specification/src/Ardalis.Specification/ISingleResultSpecification.cs
+++ Specification/src/Ardalis.Specification/ISingleResultSpecification.cs
@@ -1,7 +1,8 @@
 ﻿namespace Ardalis.Specification
 {
     /// <summary>
-    /// A marker interface for specifications that are meant to return a single entity
+    /// A marker interface for specifications that are meant to return a single entity. Used to constrain methods
+    /// that accept a Specification and return a single result rather than a collection of results
     /// </summary>
     public interface ISingleResultSpecification
     {
