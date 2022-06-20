f9c888ea3ce2ee4df899b41aee61efdf0ce5a74a
Fati Iseni <fati.iseni@pozitrongroup.com>
2022-06-20 11:03:44 +0200

    Update XML comment.

--- Specification/src/Ardalis.Specification/SingleResultSpecification.cs
+++ Specification/src/Ardalis.Specification/SingleResultSpecification.cs
@@ -1,17 +1,11 @@
 ﻿namespace Ardalis.Specification
 {
-  /// <summary>
-  /// A marker interface for specifications that are meant to return a single entity. Used to constrain methods
-  /// that accept a Specification and return a single result rather than a collection of results.
-  /// </summary>
+  /// <inheritdoc cref="ISingleResultSpecification{T}"/>
   public class SingleResultSpecification<T> : Specification<T>, ISingleResultSpecification<T>
   {
   }
 
-  /// <summary>
-  /// A marker interface for specifications that are meant to return a single entity. Used to constrain methods
-  /// that accept a Specification and return a single result rather than a collection of results.
-  /// </summary>
+  /// <inheritdoc cref="ISingleResultSpecification{T, TResult}"/>
   public class SingleResultSpecification<T, TResult> : Specification<T, TResult>, ISingleResultSpecification<T, TResult>
   {
   }
