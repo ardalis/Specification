e4445afbe6f4d741258cd609918c79cf396ec24e
Fati Iseni <fati.iseni@pozitrongroup.com>
2022-06-23 15:30:48 +0200

    Added WithSpecification overload for specifications with Select. (#273)

--- Specification.EntityFramework6/src/Ardalis.Specification.EntityFramework6/Extensions/DbSetExtensions.cs
+++ Specification.EntityFramework6/src/Ardalis.Specification.EntityFramework6/Extensions/DbSetExtensions.cs
@@ -8,7 +8,11 @@ namespace Ardalis.Specification.EntityFramework6
 {
   public static class DbSetExtensions
   {
-    public static async Task<List<TSource>> ToListAsync<TSource>(this DbSet<TSource> source, ISpecification<TSource> specification, CancellationToken cancellationToken = default) where TSource : class
+    public static async Task<List<TSource>> ToListAsync<TSource>(
+      this DbSet<TSource> source,
+      ISpecification<TSource> specification,
+      CancellationToken cancellationToken = default)
+      where TSource : class
     {
       var result = await SpecificationEvaluator.Default.GetQuery(source, specification).ToListAsync(cancellationToken);
 
@@ -17,7 +21,11 @@ namespace Ardalis.Specification.EntityFramework6
           : specification.PostProcessingAction(result).ToList();
     }
 
-    public static async Task<IEnumerable<TSource>> ToEnumerableAsync<TSource>(this DbSet<TSource> source, ISpecification<TSource> specification, CancellationToken cancellationToken = default) where TSource : class
+    public static async Task<IEnumerable<TSource>> ToEnumerableAsync<TSource>(
+      this DbSet<TSource> source,
+      ISpecification<TSource> specification,
+      CancellationToken cancellationToken = default)
+      where TSource : class
     {
       var result = await SpecificationEvaluator.Default.GetQuery(source, specification).ToListAsync(cancellationToken);
 
@@ -26,7 +34,21 @@ namespace Ardalis.Specification.EntityFramework6
           : specification.PostProcessingAction(result);
     }
 
-    public static IQueryable<TSource> WithSpecification<TSource>(this IQueryable<TSource> source, ISpecification<TSource> specification, ISpecificationEvaluator evaluator = null) where TSource : class
+    public static IQueryable<TSource> WithSpecification<TSource>(
+      this IQueryable<TSource> source,
+      ISpecification<TSource> specification,
+      ISpecificationEvaluator evaluator = null)
+      where TSource : class
+    {
+      evaluator = evaluator ?? SpecificationEvaluator.Default;
+      return evaluator.GetQuery(source, specification);
+    }
+
+    public static IQueryable<TResult> WithSpecification<TSource, TResult>(
+      this IQueryable<TSource> source,
+      ISpecification<TSource, TResult> specification,
+      ISpecificationEvaluator evaluator = null)
+      where TSource : class
     {
       evaluator = evaluator ?? SpecificationEvaluator.Default;
       return evaluator.GetQuery(source, specification);
