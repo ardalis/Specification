95a85b2051262998b8a37713192e0cf6ea32e651
Fati Iseni <fati.iseni@pozitrongroup.com>
2023-08-05 21:29:28 +0200

    Updated connection retry count.

--- Specification.EntityFramework6/tests/Ardalis.Specification.EntityFramework6.IntegrationTests/Fixture/DatabaseFixture.cs
+++ Specification.EntityFramework6/tests/Ardalis.Specification.EntityFramework6.IntegrationTests/Fixture/DatabaseFixture.cs
@@ -15,8 +15,8 @@ public class DatabaseFixture : IDisposable
         using (var localDB = new SqlLocalDbApi())
         {
             ConnectionString = localDB.IsLocalDBInstalled()
-                ? $"Data Source=(localdb)\\mssqllocaldb;Initial Catalog={databaseName};Integrated Security=SSPI;ConnectRetryCount=0;"
-                : $"Data Source=databaseEF;Initial Catalog={databaseName};PersistSecurityInfo=True;User ID=sa;Password=P@ssW0rd!;ConnectRetryCount=0;";
+                ? $"Data Source=(localdb)\\mssqllocaldb;Initial Catalog={databaseName};Integrated Security=SSPI;ConnectRetryCount=1;"
+                : $"Data Source=databaseEF;Initial Catalog={databaseName};PersistSecurityInfo=True;User ID=sa;Password=P@ssW0rd!;ConnectRetryCount=1;";
         }
 
         Console.WriteLine($"Connection string: {ConnectionString}");
