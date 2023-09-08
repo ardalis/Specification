a2698eb5f905279fd2207d2348e9660a055898b6
Thorsten <8270650+thorstenalpers@users.noreply.github.com>
2023-09-08 11:18:22 +0200

    Talpers/delete range by spec (#369)
    * Impl DeleteRangeAsync by spec
    
    * Add unit tests for DeleteRange by spec
    
    * Always delete other entities
    
    * Remove test with deletion of untracked entities
    
    ---------
    
    Co-authored-by: thorstenalpers <thorstenalpers@foo.de>

---
+++ Specification.EntityFramework6/tests/Ardalis.Specification.EntityFramework6.IntegrationTests/RepositoryOfT_DeleteRangeAsync.cs
@@ -0,0 +1,71 @@
+﻿using Ardalis.Specification.EntityFramework6.IntegrationTests.Fixture;
+using Ardalis.Specification.UnitTests.Fixture.Entities;
+using Ardalis.Specification.UnitTests.Fixture.Specs;
+using FluentAssertions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ardalis.Specification.EntityFramework6.IntegrationTests;
+
+[Collection("WriteCollection")]
+public class RepositoryOfT_DeleteRangeAsync
+{
+    private readonly string _connectionString;
+
+    public RepositoryOfT_DeleteRangeAsync(DatabaseFixture fixture)
+    {
+        _connectionString = fixture.ConnectionString;
+    }
+
+    [Fact]
+    public virtual async Task DeletesProductWithStoreIdOne_GivenProductByStoreIdSpec()
+    {
+        using var dbContext = new TestDbContext(_connectionString);
+        var repo = new Repository<Product>(dbContext, SpecificationEvaluator.Default);
+
+        await repo.DeleteRangeAsync(new ProductByStoreIdSpec(1));
+
+        var products = await repo.ListAsync();
+        products.Should().NotBeNullOrEmpty();
+        products.Should().NotContain(e => e.StoreId == 1);
+    }
+
+    [Fact]
+    public virtual async Task DeletesProductWithIdOne_GivenProductByIdSpec()
+    {
+        using var dbContext = new TestDbContext(_connectionString);
+        var repo = new Repository<Product>(dbContext, SpecificationEvaluator.Default);
+
+        await repo.DeleteRangeAsync(new ProductByIdSpec(2));
+
+        var products = await repo.ListAsync();
+        products.Should().NotBeNullOrEmpty();
+        products.Should().NotContain(e => e.Id == 2);
+    }
+
+    [Fact]
+    public virtual async Task DeletesProductWithIdOne_GivenProductByIdAsUntrackedWithIdentityResolutionSpec()
+    {
+        using var dbContext = new TestDbContext(_connectionString);
+        var repo = new Repository<Product>(dbContext, SpecificationEvaluator.Default);
+
+        await repo.DeleteRangeAsync(new ProductByIdAsUntrackedWithIdentityResolutionSpec(3));
+
+        var products = await repo.ListAsync();
+        products.Should().NotBeNullOrEmpty();
+        products.Should().NotContain(e => e.Id == 3);
+    }
+
+    [Fact]
+    public virtual async Task DeletesProductWithIdOne_GivenProductByIdAsTrackedSpec()
+    {
+        using var dbContext = new TestDbContext(_connectionString);
+        var repo = new Repository<Product>(dbContext, SpecificationEvaluator.Default);
+
+        await repo.DeleteRangeAsync(new ProductByIdAsTrackedSpec(4));
+
+        var products = await repo.ListAsync();
+        products.Should().NotBeNullOrEmpty();
+        products.Should().NotContain(e => e.Id == 4);
+    }
+}
