b11148c04fce88499ece2e73658493de675afb1c
Steve Smith <steve@kentsmiths.com>
2023-12-11 12:20:57 -0500

    Adding an example showing reuse of predicates

---
+++ sample/Ardalis.Sample.Domain/Specs/AdultCustomersByNameSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+
+namespace Ardalis.Sample.Domain.Specs;
+
+public class AdultCustomersByNameSpec : Specification<Customer>
+{
+    public AdultCustomersByNameSpec(string nameSubstring)
+    {
+        Query.Where(c => CustomerPredicates.IsAdult(c) &&
+             CustomerPredicates.NameIncludes(c, nameSubstring));
+    }
+}
---
+++ sample/Ardalis.Sample.Domain/Specs/CustomerPredicates.cs
@@ -0,0 +1,9 @@
+﻿namespace Ardalis.Sample.Domain.Specs;
+
+public static class CustomerPredicates
+{
+    public static Func<Customer, bool> IsAdult => customer => customer.Age >= 18;
+    public static Func<Customer, int, bool> IsAtLeastYearsOld => (customer, years) => customer.Age >= years;
+    public static Func<Customer, string, bool> NameIncludes => 
+        (customer, nameString) => customer.Name.Contains(nameString, StringComparison.CurrentCultureIgnoreCase);
+}
