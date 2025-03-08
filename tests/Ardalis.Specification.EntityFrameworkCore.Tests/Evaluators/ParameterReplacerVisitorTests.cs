35f3758e77d6f5d41c78d0177445a8d589d5511f
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-03-08 19:41:38 +0200

    Optimized EF Search evaluator. (#444)

--- tests/Ardalis.Specification.EntityFrameworkCore.Tests/Evaluators/ParameterReplacerVisitorTests.cs
+++ tests/Ardalis.Specification.EntityFrameworkCore.Tests/Evaluators/ParameterReplacerVisitorTests.cs
@@ -1,21 +1,21 @@
-﻿//using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 
-//namespace Tests.Evaluators;
+namespace Tests.Evaluators;
 
-//public class ParameterReplacerVisitorTests
-//{
-//    [Fact]
-//    public void ReturnsExpressionWithReplacedParameter()
-//    {
-//        Expression<Func<int, decimal, bool>> expected = (y, z) => y == 1;
+public class ParameterReplacerVisitorTests
+{
+    [Fact]
+    public void ReturnsExpressionWithReplacedParameter()
+    {
+        Expression<Func<int, decimal, bool>> expected = (y, z) => y == 1;
 
-//        Expression<Func<int, decimal, bool>> expression = (x, z) => x == 1;
-//        var oldParameter = expression.Parameters[0];
-//        var newExpression = Expression.Parameter(typeof(int), "y");
+        Expression<Func<int, decimal, bool>> expression = (x, z) => x == 1;
+        var oldParameter = expression.Parameters[0];
+        var newExpression = Expression.Parameter(typeof(int), "y");
 
-//        var visitor = new ParameterReplacerVisitor(oldParameter, newExpression);
-//        var result = visitor.Visit(expression);
+        var visitor = new ParameterReplacerVisitor(oldParameter, newExpression);
+        var result = visitor.Visit(expression);
 
-//        result.ToString().Should().Be(expected.ToString());
-//    }
-//}
+        result.ToString().Should().Be(expected.ToString());
+    }
+}
