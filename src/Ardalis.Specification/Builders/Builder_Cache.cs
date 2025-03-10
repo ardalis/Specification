0a3ee270de2eb1c1a8fd4b15522fd3e3ffdea1ee
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-03-10 17:08:01 +0200

    Add WithCacheKey extensions. (#452)

--- src/Ardalis.Specification/Builders/Builder_Cache.cs
+++ src/Ardalis.Specification/Builders/Builder_Cache.cs
@@ -93,4 +93,78 @@ public static partial class SpecificationBuilderExtensions
         Specification<T>.IsChainDiscarded = !condition;
         return (SpecificationBuilder<T>)builder;
     }
+
+    /// <summary>
+    /// Sets the cache key for the specification.
+    /// </summary>
+    /// <typeparam name="T">The type of the entity.</typeparam>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    /// <param name="builder">The specification builder.</param>
+    /// <param name="cacheKey">The cache key to be used.</param>
+    /// <returns>The updated ordered specification builder.</returns>
+    /// <exception cref="ArgumentException">If specificationName is null or empty.</exception>
+    public static ICacheSpecificationBuilder<T, TResult> WithCacheKey<T, TResult>(
+        this ISpecificationBuilder<T, TResult> builder,
+        string cacheKey) where T : class
+        => WithCacheKey(builder, cacheKey, true);
+
+    /// <summary>
+    /// Sets the cache key for the specification.
+    /// </summary>
+    /// <typeparam name="T">The type of the entity.</typeparam>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    /// <param name="builder">The specification builder.</param>
+    /// <param name="cacheKey">The cache key to be used.</param>
+    /// <param name="condition">The condition to evaluate.</param>
+    /// <returns>The updated ordered specification builder.</returns>
+    /// <exception cref="ArgumentException">If specificationName is null or empty.</exception>
+    public static ICacheSpecificationBuilder<T, TResult> WithCacheKey<T, TResult>(
+        this ISpecificationBuilder<T, TResult> builder,
+        string cacheKey,
+        bool condition) where T : class
+    {
+        if (condition)
+        {
+            builder.Specification.CacheKey = cacheKey;
+        }
+
+        Specification<T, TResult>.IsChainDiscarded = !condition;
+        return (SpecificationBuilder<T, TResult>)builder;
+    }
+
+    /// <summary>
+    /// Sets the cache key for the specification.
+    /// </summary>
+    /// <typeparam name="T">The type of the entity.</typeparam>
+    /// <param name="builder">The specification builder.</param>
+    /// <param name="cacheKey">The cache key to be used.</param>
+    /// <returns>The updated ordered specification builder.</returns>
+    /// <exception cref="ArgumentException">If specificationName is null or empty.</exception>
+    public static ICacheSpecificationBuilder<T> WithCacheKey<T>(
+        this ISpecificationBuilder<T> builder,
+        string cacheKey) where T : class
+        => WithCacheKey(builder, cacheKey, true);
+
+    /// <summary>
+    /// Sets the cache key for the specification.
+    /// </summary>
+    /// <typeparam name="T">The type of the entity.</typeparam>
+    /// <param name="builder">The specification builder.</param>
+    /// <param name="cacheKey">The cache key to be used.</param>
+    /// <param name="condition">The condition to evaluate.</param>
+    /// <returns>The updated ordered specification builder.</returns>
+    /// <exception cref="ArgumentException">If specificationName is null or empty.</exception>
+    public static ICacheSpecificationBuilder<T> WithCacheKey<T>(
+        this ISpecificationBuilder<T> builder,
+        string cacheKey,
+        bool condition) where T : class
+    {
+        if (condition)
+        {
+            builder.Specification.CacheKey = cacheKey;
+        }
+
+        Specification<T>.IsChainDiscarded = !condition;
+        return (SpecificationBuilder<T>)builder;
+    }
 }
---
+++ tests/Ardalis.Specification.Tests/Builders/Builder_Cache.cs
@@ -0,0 +1,108 @@
+﻿namespace Tests.Builders;
+
+public class Builder_Cache
+{
+    public record Customer(int Id, string Name);
+
+    [Fact]
+    public void DoesNothing_GivenNoEnableCache()
+    {
+        var spec1 = new Specification<Customer>();
+        var spec2 = new Specification<Customer, string>();
+
+        spec1.CacheKey.Should().BeNull();
+        spec1.CacheEnabled.Should().BeFalse();
+
+        spec2.CacheKey.Should().BeNull();
+        spec2.CacheEnabled.Should().BeFalse();
+    }
+
+    [Fact]
+    public void DoesNothing_GivenEnableCacheWithFalseCondition()
+    {
+        var spec1 = new Specification<Customer>();
+        spec1.Query
+            .EnableCache("asd", false);
+
+        var spec2 = new Specification<Customer, string>();
+        spec2.Query
+            .EnableCache("asd", false);
+
+        spec1.CacheKey.Should().BeNull();
+        spec1.CacheEnabled.Should().BeFalse();
+
+        spec2.CacheKey.Should().BeNull();
+        spec2.CacheEnabled.Should().BeFalse();
+    }
+
+    [Fact]
+    public void SetsCacheKey_GivenEnableCache()
+    {
+        var spec1 = new Specification<Customer>();
+        spec1.Query
+            .EnableCache("asd", "x", "y");
+
+        var spec2 = new Specification<Customer, string>();
+        spec2.Query
+            .EnableCache("asd", "x", "y");
+
+        spec1.CacheKey.Should().Be("asd-x-y");
+        spec1.CacheEnabled.Should().BeTrue();
+
+        spec1.CacheKey.Should().Be("asd-x-y");
+        spec2.CacheEnabled.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DoesNothing_GivenNoWithCacheKey()
+    {
+        var spec1 = new Specification<Customer>();
+        var spec2 = new Specification<Customer, string>();
+
+        spec1.CacheKey.Should().BeNull();
+        spec1.CacheEnabled.Should().BeFalse();
+
+        spec2.CacheKey.Should().BeNull();
+        spec2.CacheEnabled.Should().BeFalse();
+    }
+
+    [Fact]
+    public void DoesNothing_GivenWithCacheKeyWithFalseCondition()
+    {
+        var key = "someKey";
+
+        var spec1 = new Specification<Customer>();
+        spec1.Query
+            .WithCacheKey(key, false);
+
+        var spec2 = new Specification<Customer, string>();
+        spec2.Query
+            .WithCacheKey(key, false);
+
+        spec1.CacheKey.Should().BeNull();
+        spec1.CacheEnabled.Should().BeFalse();
+
+        spec2.CacheKey.Should().BeNull();
+        spec2.CacheEnabled.Should().BeFalse();
+    }
+
+    [Fact]
+    public void SetsCacheKey_GivenWithCacheKey()
+    {
+        var key = "someKey";
+
+        var spec1 = new Specification<Customer>();
+        spec1.Query
+            .WithCacheKey(key);
+
+        var spec2 = new Specification<Customer, string>();
+        spec2.Query
+            .WithCacheKey(key);
+
+        spec1.CacheKey.Should().Be(key);
+        spec1.CacheEnabled.Should().BeTrue();
+
+        spec1.CacheKey.Should().Be(key);
+        spec2.CacheEnabled.Should().BeTrue();
+    }
+}
