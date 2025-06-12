940e885595231e2bc01f001093189adef7dd8c88
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-06-12 08:19:34 +0200

    Add benchmark - IQueryable evaluation.

---
+++ tests/Ardalis.Specification.Benchmarks/Benchmark1_IQueryable.cs
@@ -0,0 +1,152 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+
+namespace Ardalis.Specification.Benchmarks;
+
+[MemoryDiagnoser]
+public class Benchmark1_IQueryable
+{
+    /* This benchmark measures building the IQueryable state. It's the pure overhead of using specifications.
+     * Types:
+     * 0 -> Empty
+     * 1 -> Single Where clause
+     * 2 -> Where and OrderBy
+     * 3 -> Where, OrderBy, Include
+     * 4 -> Where, Order chain, Include chain, Flag (AsNoTracking)
+     * 5 -> Where, Order chain, Include chain, Like, Skip, Take, Flag (AsNoTracking)
+     */
+
+    private DbSet<Store> _queryable = default!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _queryable = new BenchmarkDbContext().Stores;
+    }
+
+    [Params(0, 1, 2, 3, 4, 5)]
+    public int Type { get; set; }
+
+    [Benchmark(Baseline = true)]
+    public object EFCore()
+    {
+        if (Type == 0)
+        {
+            return _queryable;
+        }
+        else if (Type == 1)
+        {
+            return _queryable
+                .Where(x => x.Id > 0);
+        }
+        else if (Type == 2)
+        {
+            return _queryable
+                .Where(x => x.Id > 0)
+                .OrderBy(x => x.Id);
+        }
+        else if (Type == 3)
+        {
+            return _queryable
+                .Where(x => x.Id > 0)
+                .OrderBy(x => x.Id)
+                .Include(x => x.Company);
+        }
+        else if (Type == 4)
+        {
+            return _queryable
+                .Where(x => x.Id > 0)
+                .OrderBy(x => x.Id)
+                    .ThenBy(x => x.Name)
+                .Include(x => x.Company)
+                    .ThenInclude(x => x.Country)
+                .AsNoTracking();
+        }
+        else
+        {
+            var name = "%tore%";
+            return _queryable
+                .Where(x => x.Id > 0)
+                .Where(x => EF.Functions.Like(x.Name, name))
+                .OrderBy(x => x.Id)
+                    .ThenBy(x => x.Name)
+                .Include(x => x.Company)
+                    .ThenInclude(x => x.Country)
+                .Skip(1)
+                .Take(1)
+                .AsNoTracking();
+        }
+    }
+
+    [Benchmark]
+    public object Spec()
+    {
+        if (Type == 0)
+        {
+            var spec = new Specification<Store>();
+            return _queryable
+                .WithSpecification(spec);
+        }
+        else if (Type == 1)
+        {
+            var spec = new Specification<Store>();
+            spec.Query
+                .Where(x => x.Id > 0);
+
+            return _queryable
+                .WithSpecification(spec);
+        }
+        else if (Type == 2)
+        {
+            var spec = new Specification<Store>();
+            spec.Query
+                .Where(x => x.Id > 0)
+                .OrderBy(x => x.Id);
+
+            return _queryable
+                .WithSpecification(spec);
+        }
+        else if (Type == 3)
+        {
+            var spec = new Specification<Store>();
+            spec.Query
+                .Where(x => x.Id > 0)
+                .OrderBy(x => x.Id)
+                .Include(x => x.Company);
+
+            return _queryable
+                .WithSpecification(spec);
+        }
+        else if (Type == 4)
+        {
+            var spec = new Specification<Store>();
+            spec.Query
+                .Where(x => x.Id > 0)
+                .OrderBy(x => x.Id)
+                    .ThenBy(x => x.Name)
+                .Include(x => x.Company)
+                    .ThenInclude(x => x.Country)
+                .AsNoTracking();
+
+            return _queryable
+                .WithSpecification(spec);
+        }
+        else
+        {
+            var name = "%tore%";
+            var spec = new Specification<Store>();
+            spec.Query
+                .Where(x => x.Id > 0)
+                .Search(x => x.Name, name)
+                .OrderBy(x => x.Id)
+                    .ThenBy(x => x.Name)
+                .Include(x => x.Company)
+                    .ThenInclude(x => x.Country)
+                .Skip(1)
+                .Take(1)
+                .AsNoTracking();
+
+            return _queryable
+                .WithSpecification(spec);
+        }
+    }
+}
