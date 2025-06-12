933dbc302fe5bf5e22c1590ce3936fa17eeb615c
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-06-12 08:40:24 +0200

    Add benchmark - Include.

---
+++ tests/Ardalis.Specification.Benchmarks/Benchmark5_Include.cs
@@ -0,0 +1,36 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+
+namespace Ardalis.Specification.Benchmarks;
+
+[MemoryDiagnoser]
+public class Benchmark5_Include
+{
+    private DbSet<Store> _queryable = default!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _queryable = new BenchmarkDbContext().Stores;
+    }
+
+    [Benchmark(Baseline = true)]
+    public object EFCore()
+    {
+        var result = _queryable
+            .Include(x => x.Company)
+            .ThenInclude(x => x.Country);
+
+        return result;
+    }
+
+    [Benchmark]
+    public object Spec()
+    {
+        var spec = new Specification<Store>();
+        spec.Query
+            .Include(x => x.Company)
+            .ThenInclude(x => x.Country);
+
+        return _queryable.WithSpecification(spec);
+    }
+}
