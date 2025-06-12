d34c80dc83a492487a0d4f44ea33496c43ca93c8
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-06-12 09:01:44 +0200

    Add benchmark - Search Validator.

---
+++ tests/Ardalis.Specification.Benchmarks/Benchmark8_SearchMemoryValidator.cs
@@ -0,0 +1,80 @@
+﻿namespace Ardalis.Specification.Benchmarks;
+
+[MemoryDiagnoser]
+public class Benchmark8_SearchMemoryValidator
+{
+    private List<Customer> _source = default!;
+    private CustomerSpec _specification = default!;
+    private SearchValidatorV8 _validatorV8 = default!;
+    private SearchValidator _validator = default!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _source =
+        [
+            new(1, "axxa", "axya"),
+            new(2, "aaaa", "aaaa"),
+            new(3, "axxa", "axza"),
+            new(4, "aaaa", null),
+            new(5, "axxa", null),
+            .. Enumerable.Range(6, 1000).Select(x => new Customer(x, "axxa", "axya"))
+        ];
+
+        _specification = new CustomerSpec();
+        _validator = SearchValidator.Instance;
+        _validatorV8 = new SearchValidatorV8();
+    }
+
+    [Benchmark(Baseline = true)]
+    public bool ValidateV8()
+    {
+        var validator = _validatorV8;
+
+        var result = false;
+        foreach (var item in _source)
+        {
+            result = validator.IsValid(item, _specification);
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public bool Validate()
+    {
+        var validator = _validator;
+
+        var result = false;
+        foreach (var item in _source)
+        {
+            result = validator.IsValid(item, _specification);
+        }
+        return result;
+    }
+
+    private record Customer(int Id, string FirstName, string? LastName);
+    private class CustomerSpec : Specification<Customer>
+    {
+        public CustomerSpec()
+        {
+            Query
+                .Search(x => x.FirstName, "%xx%", 1)
+                .Search(x => x.LastName, "%xy%", 2)
+                .Search(x => x.LastName, "%xz%", 2);
+        }
+    }
+
+    private sealed class SearchValidatorV8 : IValidator
+    {
+        public bool IsValid<T>(T entity, ISpecification<T> specification)
+        {
+            foreach (var searchGroup in specification.SearchCriterias.GroupBy(x => x.SearchGroup))
+            {
+                if (searchGroup.Any(c => c.SelectorFunc(entity)?.Like(c.SearchTerm) ?? false) == false) return false;
+            }
+
+            return true;
+        }
+    }
+}
+
