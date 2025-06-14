7c24284a10822381d9c83b6537a1def2c76d1976
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-06-14 22:41:00 +0200

    Add XML comments.

--- src/Ardalis.Specification/Exceptions/ConcurrentSelectorsException.cs
+++ src/Ardalis.Specification/Exceptions/ConcurrentSelectorsException.cs
@@ -1,14 +1,24 @@
 ﻿namespace Ardalis.Specification;
 
+/// <summary>
+/// Exception thrown when concurrent selector transforms are defined in a specification.
+/// </summary>
 public class ConcurrentSelectorsException : Exception
 {
     private const string MESSAGE = "Concurrent specification selector transforms defined. Ensure only one of the Select() or SelectMany() transforms is used in the same specification!";
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConcurrentSelectorsException"/> class.
+    /// </summary>
     public ConcurrentSelectorsException()
         : base(MESSAGE)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConcurrentSelectorsException"/> class with a specified inner exception.
+    /// </summary>
+    /// <param name="innerException">The exception that is the cause of the current exception.</param>
     public ConcurrentSelectorsException(Exception innerException)
         : base(MESSAGE, innerException)
     {
