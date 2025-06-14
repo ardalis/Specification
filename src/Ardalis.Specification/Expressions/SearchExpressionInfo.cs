6a1c02c33a8bccdbb7c030fd4f6f183f7a04da8a
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-06-14 20:42:45 +0200

    Add tests for SearchExpressionComparer.

--- src/Ardalis.Specification/Expressions/SearchExpressionInfo.cs
+++ src/Ardalis.Specification/Expressions/SearchExpressionInfo.cs
@@ -46,17 +46,3 @@ public class SearchExpressionInfo<T>
     /// </summary>
     public Func<T, string?> SelectorFunc => _selectorFunc ??= Selector.Compile();
 }
-
-internal sealed class SearchExpressionComparer<T> : IComparer<SearchExpressionInfo<T>>
-{
-    public static readonly SearchExpressionComparer<T> Default = new();
-    private SearchExpressionComparer() { }
-
-    public int Compare(SearchExpressionInfo<T>? x, SearchExpressionInfo<T>? y)
-    {
-        if (ReferenceEquals(x, y)) return 0;
-        if (x is null) return -1;
-        if (y is null) return 1;
-        return x.SearchGroup.CompareTo(y.SearchGroup);
-    }
-}
---
+++ tests/Ardalis.Specification.Tests/Internals/SearchExpressionComparerTests.cs
@@ -0,0 +1,60 @@
+﻿namespace Tests.Internals;
+
+public class SearchExpressionComparerTests
+{
+    public record Customer(int Id, string Name);
+
+    private static SearchExpressionInfo<Customer> Create(int group) =>
+        new(x => x.Name, $"term{group}", group);
+
+    [Fact]
+    public void Compare_ReturnsZero_WhenSameReference()
+    {
+        var info = Create(1);
+        var comparer = SearchExpressionComparer<Customer>.Default;
+        comparer.Compare(info, info).Should().Be(0);
+    }
+
+    [Fact]
+    public void Compare_ReturnsNegative_WhenXIsNull()
+    {
+        var y = Create(1);
+        var comparer = SearchExpressionComparer<Customer>.Default;
+        comparer.Compare(null, y).Should().BeLessThan(0);
+    }
+
+    [Fact]
+    public void Compare_ReturnsPositive_WhenYIsNull()
+    {
+        var x = Create(1);
+        var comparer = SearchExpressionComparer<Customer>.Default;
+        comparer.Compare(x, null).Should().BeGreaterThan(0);
+    }
+
+    [Fact]
+    public void Compare_ReturnsNegative_WhenXGroupLessThanYGroup()
+    {
+        var x = Create(1);
+        var y = Create(2);
+        var comparer = SearchExpressionComparer<Customer>.Default;
+        comparer.Compare(x, y).Should().BeLessThan(0);
+    }
+
+    [Fact]
+    public void Compare_ReturnsPositive_WhenXGroupGreaterThanYGroup()
+    {
+        var x = Create(3);
+        var y = Create(2);
+        var comparer = SearchExpressionComparer<Customer>.Default;
+        comparer.Compare(x, y).Should().BeGreaterThan(0);
+    }
+
+    [Fact]
+    public void Compare_ReturnsZero_WhenGroupsAreEqualButDifferentInstances()
+    {
+        var x = Create(2);
+        var y = Create(2);
+        var comparer = SearchExpressionComparer<Customer>.Default;
+        comparer.Compare(x, y).Should().Be(0);
+    }
+}
