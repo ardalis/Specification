11108d6a58e70a1dc1abfde63987d865a053fb73
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-08-23 13:57:07 +0200

    Add tests for custom specs.

---
+++ tests/Ardalis.Specification.Tests/Evaluators/WhereEvaluatorCustomSpecTests.cs
@@ -0,0 +1,85 @@
+﻿namespace Tests.Evaluators;
+
+public class WhereEvaluatorCustomSpecTests
+{
+    private static readonly WhereEvaluator _evaluator = WhereEvaluator.Instance;
+
+    public record Customer(int Id);
+
+    [Fact]
+    public void Filters_GivenSingleWhereExpression()
+    {
+        List<Customer> input = [new(1), new(2), new(3), new(4), new(5)];
+        List<Customer> expected = [new(4), new(5)];
+
+        var spec = new CustomSpecification<Customer>();
+        spec.Where.Add(new WhereExpressionInfo<Customer>(x => x.Id > 3));
+
+        Assert(spec, input, expected);
+    }
+
+    [Fact]
+    public void Filters_GivenMultipleWhereExpressions()
+    {
+        List<Customer> input = [new(1), new(2), new(3), new(4), new(5)];
+        List<Customer> expected = [new(4)];
+
+        var spec = new CustomSpecification<Customer>();
+        spec.Where.Add(new WhereExpressionInfo<Customer>(x => x.Id > 3));
+        spec.Where.Add(new WhereExpressionInfo<Customer>(x => x.Id < 5));
+
+        Assert(spec, input, expected);
+    }
+
+    [Fact]
+    public void DoesNotFilter_GivenNoWhereExpression()
+    {
+        List<Customer> input = [new(1), new(2), new(3), new(4), new(5)];
+        List<Customer> expected = [new(1), new(2), new(3), new(4), new(5)];
+
+        var spec = new CustomSpecification<Customer>();
+
+        Assert(spec, input, expected);
+    }
+
+    private static void Assert<T>(ISpecification<T> spec, List<T> input, List<T> expected) where T : class
+    {
+        var actualForIEnumerable = _evaluator.Evaluate(input, spec);
+        actualForIEnumerable.Should().NotBeNull();
+        actualForIEnumerable.Should().Equal(expected);
+
+        var actualForIQueryable = _evaluator.GetQuery(input.AsQueryable(), spec);
+        actualForIQueryable.Should().NotBeNull();
+        actualForIQueryable.Should().Equal(expected);
+    }
+
+    public class CustomSpecification<T> : ISpecification<T>
+    {
+        public List<WhereExpressionInfo<T>> Where { get; set; } = new();
+        public List<SearchExpressionInfo<T>> Search { get; set; } = new();
+        public IEnumerable<SearchExpressionInfo<T>> SearchCriterias => Search;
+        public IEnumerable<WhereExpressionInfo<T>> WhereExpressions => Where;
+
+        public ISpecificationBuilder<T> Query => throw new NotImplementedException();
+        public IEnumerable<OrderExpressionInfo<T>> OrderExpressions => throw new NotImplementedException();
+        public IEnumerable<IncludeExpressionInfo> IncludeExpressions => throw new NotImplementedException();
+        public IEnumerable<string> IncludeStrings => throw new NotImplementedException();
+        public Dictionary<string, object> Items => throw new NotImplementedException();
+        public int Take => throw new NotImplementedException();
+        public int Skip => throw new NotImplementedException();
+        public Func<IEnumerable<T>, IEnumerable<T>>? PostProcessingAction => throw new NotImplementedException();
+        public IEnumerable<string> QueryTags => throw new NotImplementedException();
+        public bool CacheEnabled => throw new NotImplementedException();
+        public string? CacheKey => throw new NotImplementedException();
+        public bool AsTracking => throw new NotImplementedException();
+        public bool AsNoTracking => throw new NotImplementedException();
+        public bool AsSplitQuery => throw new NotImplementedException();
+        public bool AsNoTrackingWithIdentityResolution => throw new NotImplementedException();
+        public bool IgnoreQueryFilters => throw new NotImplementedException();
+        public bool IgnoreAutoIncludes => throw new NotImplementedException();
+        public IEnumerable<T> Evaluate(IEnumerable<T> entities)
+            => throw new NotImplementedException();
+        public bool IsSatisfiedBy(T entity)
+            => throw new NotImplementedException();
+    }
+}
---
+++ tests/Ardalis.Specification.Tests/Validators/WhereValidatorCustomSpecTests.cs
@@ -0,0 +1,118 @@
+﻿namespace Tests.Validators;
+
+public class WhereValidatorCustomSpecTests
+{
+    private static readonly WhereValidator _validator = WhereValidator.Instance;
+
+    public record Customer(int Id, string Name);
+
+    [Fact]
+    public void ReturnsTrue_GivenEmptySpec()
+    {
+        var customer = new Customer(1, "Customer1");
+
+        var spec = new CustomSpecification<Customer>();
+
+        var result = _validator.IsValid(customer, spec);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ReturnsTrue_GivenSpecWithSingleWhere_WithValidEntity()
+    {
+        var customer = new Customer(1, "Customer1");
+
+        var spec = new CustomSpecification<Customer>();
+        spec.Where.Add(new WhereExpressionInfo<Customer>(x => x.Id == 1));
+
+        var result = _validator.IsValid(customer, spec);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ReturnsFalse_GivenSpecWithSingleWhere_WithInvalidEntity()
+    {
+        var customer = new Customer(1, "Customer1");
+
+        var spec = new CustomSpecification<Customer>();
+        spec.Where.Add(new WhereExpressionInfo<Customer>(x => x.Id == 2));
+
+        var result = _validator.IsValid(customer, spec);
+
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ReturnsTrue_GivenSpecWithMultipleWhere_WithValidEntity()
+    {
+        var customer = new Customer(1, "Customer1");
+
+        var spec = new CustomSpecification<Customer>();
+        spec.Where.Add(new WhereExpressionInfo<Customer>(x => x.Id == 1));
+        spec.Where.Add(new WhereExpressionInfo<Customer>(x => x.Name == "Customer1"));
+
+        var result = _validator.IsValid(customer, spec);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ReturnsFalse_GivenSpecWithMultipleWhere_WithSingleInvalidValue()
+    {
+        var customer = new Customer(1, "Customer1");
+
+        var spec = new CustomSpecification<Customer>();
+        spec.Where.Add(new WhereExpressionInfo<Customer>(x => x.Id == 2));
+        spec.Where.Add(new WhereExpressionInfo<Customer>(x => x.Name == "Customer1"));
+
+        var result = _validator.IsValid(customer, spec);
+
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ReturnsFalse_GivenSpecWithMultipleWhere_WithAllInvalidValues()
+    {
+        var customer = new Customer(1, "Customer1");
+
+        var spec = new CustomSpecification<Customer>();
+        spec.Where.Add(new WhereExpressionInfo<Customer>(x => x.Id == 1));
+        spec.Where.Add(new WhereExpressionInfo<Customer>(x => x.Name == "Customer2"));
+
+        var result = _validator.IsValid(customer, spec);
+
+        result.Should().BeFalse();
+    }
+
+    public class CustomSpecification<T> : ISpecification<T>
+    {
+        public List<WhereExpressionInfo<T>> Where { get; set; } = new();
+        public List<SearchExpressionInfo<T>> Search { get; set; } = new();
+        public IEnumerable<SearchExpressionInfo<T>> SearchCriterias => Search;
+        public IEnumerable<WhereExpressionInfo<T>> WhereExpressions => Where;
+
+        public ISpecificationBuilder<T> Query => throw new NotImplementedException();
+        public IEnumerable<OrderExpressionInfo<T>> OrderExpressions => throw new NotImplementedException();
+        public IEnumerable<IncludeExpressionInfo> IncludeExpressions => throw new NotImplementedException();
+        public IEnumerable<string> IncludeStrings => throw new NotImplementedException();
+        public Dictionary<string, object> Items => throw new NotImplementedException();
+        public int Take => throw new NotImplementedException();
+        public int Skip => throw new NotImplementedException();
+        public Func<IEnumerable<T>, IEnumerable<T>>? PostProcessingAction => throw new NotImplementedException();
+        public IEnumerable<string> QueryTags => throw new NotImplementedException();
+        public bool CacheEnabled => throw new NotImplementedException();
+        public string? CacheKey => throw new NotImplementedException();
+        public bool AsTracking => throw new NotImplementedException();
+        public bool AsNoTracking => throw new NotImplementedException();
+        public bool AsSplitQuery => throw new NotImplementedException();
+        public bool AsNoTrackingWithIdentityResolution => throw new NotImplementedException();
+        public bool IgnoreQueryFilters => throw new NotImplementedException();
+        public bool IgnoreAutoIncludes => throw new NotImplementedException();
+        public IEnumerable<T> Evaluate(IEnumerable<T> entities)
+            => throw new NotImplementedException();
+        public bool IsSatisfiedBy(T entity)
+            => throw new NotImplementedException();
+    }
+}
